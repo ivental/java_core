fbad4ddd0c813ba327b79d9fadaf83baa9ebc23a
Илья Юкин <iventalll@gmail.com>
2025-10-03 13:27:41 +0300

    MP-18: Созданы переменные.

---
+++ src/main/java/ru/mentee/power/variables/Main.java
@@ -0,0 +1,10 @@
+package ru.mentee.power.variables;
+
+public class Main {
+    public static void main(String[] args) {
+        int myAge = 35;
+        String myName = "Илья";
+        System.out.println("Меня зовут " + myName + "," +  " мне " + myAge + " лет.");
+    }
+
+}
